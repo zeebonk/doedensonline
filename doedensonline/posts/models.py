83a7260f5590e7208b3ab704c46d1f0c5c8c86bc
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-14 19:46:02 +0100

    Add Post and Comment model

--- doedensonline/posts/models.py
+++ doedensonline/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
