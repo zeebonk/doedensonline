c93a4b5f337f5916a89e8b73537e32995255895b
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-14 20:34:29 +0100

    Add author to Post and Comment, add album models

--- doedensonline/albums/models.py
+++ doedensonline/albums/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="album_comments"
+    )
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
