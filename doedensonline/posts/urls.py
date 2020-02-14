7c1c7cdc33cb84d63cc24a93dde3dd6c6e6838ed
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-15 00:27:49 +0100

    Post list styling

--- doedensonline/posts/urls.py
+++ doedensonline/posts/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = "posts"
 urlpatterns = [
     path("", views.PostList.as_view(), name="list"),
+    path("create", views.PostCreate.as_view(), name="create"),
 ]
--- doedensonline/posts/views.py
+++ doedensonline/posts/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
 from .models import Post
 
@@ -6,3 +7,14 @@ from .models import Post
 class PostList(ListView):
     paginate_by = 5
     model = Post
+    ordering = ["-created_at"]
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ["message"]
+    success_url = reverse_lazy("posts:list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
