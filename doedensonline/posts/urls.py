9bab9e791e6e7273fc1f116069ea22121135e463
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-15 14:56:52 +0100

    Edit post flow

--- doedensonline/posts/urls.py
+++ doedensonline/posts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
     path("create", views.PostCreateView.as_view(), name="create"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path("<int:pk>/update", views.PostUpdateView.as_view(), name="update"),
     path("<int:pk>/delete", views.PostDeleteView.as_view(), name="delete"),
 ]
--- doedensonline/posts/views.py
+++ doedensonline/posts/views.py
@@ -1,4 +1,4 @@
-from crispy_forms.layout import HTML, ButtonHolder, Div, Field, Layout, Submit
+from crispy_forms.layout import HTML, Div, Field, Layout, Submit
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
@@ -17,6 +17,46 @@ class PostListView(BaseMixin, ListView):
 class PostDetailView(BaseMixin, DetailView):
     page_title = "Nieuwtje bekijken"
     model = Post
+
+
+class PostCreateView(BaseMixin, CreateView):
+    page_title = "Nieuwtje toevoegen"
+    model = Post
+    fields = ["message"]
+    success_url = reverse_lazy("posts:list")
+
+    form_layout = Layout(
+        Field("message"),
+        Div(
+            Submit("submit", "Toevoegen", css_class="btn btn-primary"),
+            HTML(
+                """<a href="{% url 'posts:list' %}" class="btn btn-secondary">Terug</a>"""
+            ),
+            css_class="btn-group",
+        ),
+    )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(BaseMixin, UpdateView):
+    page_title = "Nieuwtje aanpassen"
+    model = Post
+    fields = ["message"]
+    success_url = reverse_lazy("posts:list")
+
+    form_layout = Layout(
+        Field("message"),
+        Div(
+            Submit("submit", "Wijzingen opslaan", css_class="btn btn-primary"),
+            HTML(
+                """<a href="{% url 'posts:list' %}" class="btn btn-secondary">Terug</a>"""
+            ),
+            css_class="btn-group",
+        ),
+    )
 
 
 class PostDeleteView(BaseMixin, UpdateView):
@@ -38,26 +78,4 @@ class PostDeleteView(BaseMixin, UpdateView):
 
     def form_valid(self, form):
         form.instance.status = Post.Status.DELETED
-        return super().form_valid(form)
-
-
-class PostCreateView(BaseMixin, CreateView):
-    page_title = "Nieuwtje toevoegen"
-    model = Post
-    fields = ["message"]
-    success_url = reverse_lazy("posts:list")
-
-    form_layout = Layout(
-        Field("message"),
-        Div(
-            Submit("submit", "Toevoegen", css_class="btn btn-primary"),
-            HTML(
-                """<a href="{% url 'posts:list' %}" class="btn btn-secondary">Terug</a>"""
-            ),
-            css_class="btn-group"
-        )
-    )
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
         return super().form_valid(form)
