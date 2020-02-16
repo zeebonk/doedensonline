0acfe73842158a9fabbc700d566d817ad6b62e55
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-16 18:08:25 +0100

    Show cards in delete flow, check permissions

--- doedensonline/posts/templatetags/cards.py
+++ doedensonline/posts/templatetags/cards.py
@@ -5,30 +5,30 @@ register = template.Library()
 
 
 @register.inclusion_tag("posts/card.html")
-def post_card(item, user, size, details_link=False, flip=False):
+def post_card(item, user, details_link=False, flip=False, size=None, tools=True):
     user_is_author = item.author == user
     return {
         "item": item,
-        "size": size,
-        "show_tools": details_link or user_is_author,
-        "detail_view": "posts:detail" if details_link else None,
-        "update_view": "posts:update" if user_is_author else None,
-        "delete_view": "posts:delete" if user_is_author else None,
+        "show_tools": tools and (details_link or user_is_author),
+        "detail_view": "posts:detail" if tools and details_link else None,
+        "update_view": "posts:update" if tools and user_is_author else None,
+        "delete_view": "posts:delete" if tools and user_is_author else None,
         "flip": flip,
         "highlight": True,
+        "size": size,
     }
 
 
 @register.inclusion_tag("posts/card.html")
-def comment_card(item, user, size, flip=False):
+def comment_card(item, user, flip=False, size=None, tools=True):
     user_is_author = item.author == user
     return {
         "item": item,
-        "size": size,
-        "show_tools": user_is_author,
+        "show_tools": tools and user_is_author,
         "detail_view": None,
-        "update_view": "posts:update_comment" if user_is_author else None,
-        "delete_view": "posts:delete_comment" if user_is_author else None,
+        "update_view": "posts:update_comment" if tools and user_is_author else None,
+        "delete_view": "posts:delete_comment" if tools and user_is_author else None,
         "flip": flip,
         "highlight": False,
+        "size": size,
     }
