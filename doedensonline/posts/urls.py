3709f4123ee2ea9d791fe19a0828f23b621fd6fb
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-16 13:51:35 +0100

    Add: comment update flow

--- doedensonline/posts/urls.py
+++ doedensonline/posts/urls.py
@@ -16,6 +16,11 @@ urlpatterns = [
         name="add_comment",
     ),
     path(
-        "comments/<int:pk>/", views.CommentDeleteView.as_view(), name="delete_comment",
+        "comments/<int:pk>/", views.CommentUpdateView.as_view(), name="update_comment",
+    ),
+    path(
+        "comments/<int:pk>/delete",
+        views.CommentDeleteView.as_view(),
+        name="delete_comment",
     ),
 ]
