16167bc984c328b2080ececd891c23830d345b28
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-25 22:18:01 +0100

    Just use docker-compose 2.1

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM python:3.8.1-slim-buster
 
 WORKDIR /app
 
+ENV PIPENV_NOSPIN=1
+ENV PIPENV_HIDE_EMOJIS=1
+ENV PIPENV_COLORBLIND=1
+
 RUN pip install --upgrade pip
 RUN pip install pipenv
 
--- check.sh
+++ check.sh
@@ -1,4 +1,6 @@
 set -euxo pipefail
 
+pipenv run isort --check-only --diff
+pipenv run black --diff .
 pipenv run flake8
 docker-compose run --rm app pipenv run ./manage.py makemigrations --check --dry-run
