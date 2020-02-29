a23fba8905c017c83b77280e6dc39c6cad53620b
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-29 14:37:08 +0100

    Add Terraform manifests for deployment (#2)

--- src/check.sh
+++ src/check.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 pipenv run isort --check-only --diff
 pipenv run black --diff .
 pipenv run flake8
-docker-compose run --rm app pipenv run ./manage.py makemigrations --check --dry-run
+docker-compose run --rm app ./manage.py makemigrations --check --dry-run
