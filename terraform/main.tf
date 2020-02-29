a0128693e5f02ff8294f8b1ae5401e0f5eb80836
Gijs van der Voort <vandervoort.gijs@gmail.com>
2020-02-29 18:06:38 +0100

    Add build flow for Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,40 +11,40 @@ variable django_superuser_password {}
 variable django_superuser_email {}
 
 resource "heroku_app" "doedensonline" {
-    name   = "doedensonline"
-    region = "eu"
-    stack  = "container"
+  name   = "doedensonline"
+  region = "eu"
+  stack  = "container"
 
-    config_vars = {
-        DDO_DEBUG = var.ddo_debug
-        DDO_ALLOWED_HOSTS = var.ddo_allowed_hosts
-        DDO_STATIC_URL = var.ddo_static_url
-        DDO_STATIC_ROOT = var.ddo_static_root
-    }
+  config_vars = {
+    DDO_DEBUG         = var.ddo_debug
+    DDO_ALLOWED_HOSTS = var.ddo_allowed_hosts
+    DDO_STATIC_URL    = var.ddo_static_url
+    DDO_STATIC_ROOT   = var.ddo_static_root
+  }
 
-    sensitive_config_vars = {
-        DDO_SECRET_KEY = var.ddo_secret_key
-        DJANGO_SUPERUSER_USERNAME = var.django_superuser_username
-        DJANGO_SUPERUSER_PASSWORD = var.django_superuser_password
-        DJANGO_SUPERUSER_EMAIL = var.django_superuser_email
-    }
+  sensitive_config_vars = {
+    DDO_SECRET_KEY            = var.ddo_secret_key
+    DJANGO_SUPERUSER_USERNAME = var.django_superuser_username
+    DJANGO_SUPERUSER_PASSWORD = var.django_superuser_password
+    DJANGO_SUPERUSER_EMAIL    = var.django_superuser_email
+  }
 }
 
 resource "heroku_build" "doedensonline" {
-    app    = heroku_app.doedensonline.id
-    source = {
-        path = "../src"
-    }
+  app = heroku_app.doedensonline.id
+  source = {
+    path = "../src"
+  }
 }
 
 resource "heroku_formation" "doedensonline" {
-    app      = heroku_app.doedensonline.id
-    type     = "web"
-    quantity = 1
-    size     = "free"
+  app      = heroku_app.doedensonline.id
+  type     = "web"
+  quantity = 1
+  size     = "free"
 }
 
 resource "heroku_addon" "doedensonline" {
-    app  = heroku_app.doedensonline.name
-    plan = "heroku-postgresql:hobby-dev"
+  app  = heroku_app.doedensonline.name
+  plan = "heroku-postgresql:hobby-dev"
 }
