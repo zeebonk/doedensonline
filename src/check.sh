b353315cc53489d71c4ca6f7d2fe4b5684e47ce0
Gijs van der Voort <vandervoort.gijs@gmail.com>
2021-03-06 12:50:57 +0100

    Better manage

--- src/check.sh
+++ src/check.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 poetry run isort . --check --diff
 poetry run black . --diff
 poetry run flake8
-docker-compose run --rm app ./manage.py makemigrations --check --dry-run
+docker-compose run --rm app poetry run manage makemigrations --check --dry-run
