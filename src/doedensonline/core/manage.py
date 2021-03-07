1aea6d66e5ee3fbb7d836d7aaaa5882420317c88
Gijs van der Voort <vandervoort.gijs@gmail.com>
2021-03-07 14:24:04 +0100

    Move manage.py to core, executable `via poetry run manage`

--- src/doedensonline/core/manage.py
+++ src/doedensonline/core/manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -15,7 +14,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
