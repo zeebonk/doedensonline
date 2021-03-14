30ecb30ba8d92abbd9c5ea39ca4258f5b9e0062f
Gijs van der Voort <vandervoort.gijs@gmail.com>
2021-03-14 22:36:25 +0100

    Light styling, delete modals, home content (#18)

--- src/doedensonline/posts/templatetags/cards.py
+++ src/doedensonline/posts/templatetags/cards.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 @register.inclusion_tag("posts/card.html")
-def post_card(item, user, details_link=False, flip=False, size=None, tools=True):
+def post_card(item, user, details_link=False, tools=True):
     user_is_author = item.author == user
     return {
         "item": item,
@@ -13,14 +13,16 @@ def post_card(item, user, details_link=False, flip=False, size=None, tools=True)
         "detail_view": "posts:detail" if tools and details_link else None,
         "update_view": "posts:update" if tools and user_is_author else None,
         "delete_view": "posts:delete" if tools and user_is_author else None,
-        "flip": flip,
-        "highlight": True,
-        "size": size,
+        "delete_title": "Nieuwtje verwijderen",
+        "delete_message": "U staat op het punt uw nieuwtje, en de daar onder geplaatste reacties, te verwijderen. Weet u het zeker?",
+        "header": "h3",
+        "shadow": "shadow",
+        "col": "col m-5 p-5",
     }
 
 
 @register.inclusion_tag("posts/card.html")
-def comment_card(item, user, flip=False, size=None, tools=True):
+def comment_card(item, user, tools=True):
     user_is_author = item.author == user
     return {
         "item": item,
@@ -28,7 +30,9 @@ def comment_card(item, user, flip=False, size=None, tools=True):
         "detail_view": None,
         "update_view": "posts:update_comment" if tools and user_is_author else None,
         "delete_view": "posts:delete_comment" if tools and user_is_author else None,
-        "flip": flip,
-        "highlight": False,
-        "size": size,
+        "delete_title": "Reactie verwijderen",
+        "delete_message": "U staat op het punt uw reactie te verwijderen. Weet u het zeker?",
+        "header": "h5",
+        "shadow": "shadow-sm",
+        "col": "col-sm-8 m-4 p-5",
     }
