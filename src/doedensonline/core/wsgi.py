2e922618eb57ed36f8267386aa296c8c6ede6da0
Gijs van der Voort <vandervoort.gijs@gmail.com>
2021-03-20 14:11:51 +0100

    Set up application

--- src/doedensonline/core/wsgi.py
+++ src/doedensonline/core/wsgi.py
@@ -1,17 +1,20 @@
-"""
-WSGI config for doedensonline project.
+# fmt: off
+# isort:skip_file
+# flake8: noqa
 
-It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
+# New Relic needs to be initialized before anything else
+import newrelic.agent
+from pathlib import Path
+newrelic.agent.initialize(Path(__file__).resolve().parent / "newrelic.ini")
 
+
+# Now we can continue normally
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doedensonline.core.settings")
 
 application = get_wsgi_application()
+application = newrelic.agent.WSGIApplicationWrapper(application)
