9c5b39867bc0c77a00d4ffdf6bc0c365b3f47072
Gijs van der Voort <vandervoort.gijs@gmail.com>
2021-08-12 13:48:53 +0200

    Use confirm modal for logging out (#33, #34)

--- src/doedensonline/home/views.py
+++ src/doedensonline/home/views.py
@@ -1,19 +1,12 @@
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
-from django.forms import Form
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import FormView, ListView
+from django.views.generic import ListView
 
-from doedensonline.core.layout import (
-    ButtonGroup,
-    Field,
-    Layout,
-    PrimarySubmit,
-    SecondayLink,
-)
+from doedensonline.core.layout import ButtonGroup, Field, Layout, PrimarySubmit
 from doedensonline.core.mixins import BaseMixin
 from doedensonline.posts.models import Post
 
@@ -39,19 +32,10 @@ class LoginView(BaseMixin, LoginView):
     )
 
 
-class LogoutView(BaseMixin, FormView):
-    page_title = "Uitloggen"
-    template_name = "registration/logout.html"
-    form_class = Form
-    success_url = reverse_lazy("home:index")
-
-    form_layout = Layout(
-        ButtonGroup(PrimarySubmit("Ja"), SecondayLink("Nee", "home:index")),
-    )
-
+class LogoutView(BaseMixin, View):
     def post(self, request, *args, **kwargs):
         logout(request)
-        return super().post(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse_lazy("home:index"))
 
 
 class HealthView(BaseMixin, View):
